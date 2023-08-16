6d52c773cd964dae53a287aa54b4f790d1e74b2a
amine <amine@email.com>
2023-08-16 20:36:59 +0100

    c

--- project/routing.py
+++ project/routing.py
@@ -7,9 +7,7 @@ from django.core.asgi import get_asgi_application
 
 # urls that handles the websocket connection is put here
 websocket_urlpatterns=[
-                    re_path(
-                        r"ws/chat/(?P<chat_box_name>\w+)/$", consumers.ChatRoomConsumer.as_asgi()
-                    ),
+                    re_path(r"ws/chat/(?P<chat_box_name>\w+)/$", consumers.ChatRoomConsumer.as_asgi()),
                 ]
 
 application = ProtocolTypeRouter( 
--- project/settings.py
+++ project/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     "app",
     "daphne",
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
 ]
 
 MIDDLEWARE = [
